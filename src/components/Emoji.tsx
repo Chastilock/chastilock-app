689a7a706e285358c88210245a08c5b2fc5aad60
Itsuki <itsuki@lars.ninja>
2021-02-28 23:35:36 +0100

    add all colors for emojis

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -2,9 +2,22 @@ import React from 'react'
 import Image from './Image'
 
 import mapping from '../assets/images/emojis/emojiMapping.json'
+
+import BLUE from '../assets/images/emojis/blue.png'
+import GREEN from '../assets/images/emojis/green.png'
+import ORANGE from '../assets/images/emojis/orange.png'
+import PURPLE from '../assets/images/emojis/purple.png'
+import RED from '../assets/images/emojis/red.png'
+import WHITE from '../assets/images/emojis/white.png'
 import YELLOW from '../assets/images/emojis/yellow.png'
 
 export const EMOJI_TYPE = {
+  BLUE,
+  GREEN,
+  ORANGE,
+  PURPLE,
+  RED,
+  WHITE,
   YELLOW
 }
 
