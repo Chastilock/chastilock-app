eca33d8d49ec5f2b179c725726a1e950a788b3e9
Itsuki <itsuki@lars.ninja>
2021-03-07 23:51:55 +0100

    add selection screen for migration

--- src/views/setup/CkMigration.tsx
+++ src/views/setup/CkMigration.tsx
@@ -1,22 +1,27 @@
 import React from 'react'
-import { TopNavigation, Divider, Button, Input } from '@ui-kitten/components'
+import { TopNavigation, Divider, Button, Input, CheckBox } from '@ui-kitten/components'
 import { SafeAreaView, View } from 'react-native'
 
-import { Text, useTranslation } from '@chastilock/components'
+import { Text, TextType, useTranslation } from '@chastilock/components'
 import { BackButtonAccessory } from '../common/Accessories'
 
-interface CkMigrateProps {
+interface CkMigrationProps {
   onOk: () => void
   onBack?: () => void
 }
-export const FinishSetup = (props: CkMigrateProps): React.ReactElement => {
+export const CkMigration = (props: CkMigrationProps): React.ReactElement => {
   const [translate] = useTranslation()
   const [username, setUsername] = React.useState('')
   const [transferCode, setTransferCode] = React.useState('')
+  const [isShowingSelection, setShowSelection] = React.useState(false)
 
   const migrate = (): void => {
     // TODO make api call to migrate, and only call this if okay
-    props.onOk()
+    setShowSelection(true)
+  }
+
+  if (isShowingSelection) {
+    return <CkMigrationSelection onBack={() => setShowSelection(false)} onOk={props.onOk} data={{}} />
   }
 
   return (
@@ -39,4 +44,41 @@ export const FinishSetup = (props: CkMigrateProps): React.ReactElement => {
   )
 }
 
-export default FinishSetup
+interface CkMigrationSelectionProps {
+  onBack?: () => void
+  onOk: () => void
+  data: any
+}
+export const CkMigrationSelection = (props: CkMigrationSelectionProps): React.ReactElement => {
+  const [translate] = useTranslation()
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#222B45' }}>
+      <TopNavigation
+        title={translate('setup.ck_migrate.selection.title')}
+        alignment="center"
+        accessoryLeft={() => <BackButtonAccessory onPress={() => props.onBack?.()} />}
+      />
+      <Divider />
+      <View style={{ padding: 20 }}>
+        <Text translationKey="setup.ck_migrate.selection.info" center style={{ marginBottom: 20 }} />
+
+        <Text category={TextType.HEADING3} translationKey="setup.ck_migrate.selection.lockee" />
+        <CheckBox>My active lock(s) (1 lock active)</CheckBox>
+        <Text translationKey="setup.ck_migrate.selection.kh_lock_info" />
+        <CheckBox>My lock history (23 past locks)</CheckBox>
+        <CheckBox>My ratings (4.8/5, 17 ratings)</CheckBox>
+
+        <Text category={TextType.HEADING3} translationKey="setup.ck_migrate.selection.keyholder" />
+        <CheckBox>My active lock(s) (3 locks active)</CheckBox>
+        <Text translationKey="setup.ck_migrate.selection.kh_lock_info" />
+        <CheckBox>My lock history (87 past locks)</CheckBox>
+        <CheckBox>My ratings (4.9/5, 78 ratings)</CheckBox>
+
+        <Button style={{ marginTop: 10 }} onPress={props.onOk}>{translate('setup.ck_migrate.selection.ok')}</Button>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default CkMigration
