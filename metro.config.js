a2f352968f4b483449a4380b502213ae98543d47
Havok <thetechnerd@protonmail.com>
2021-11-26 18:33:34 +0000

    Testing without watch config

--- metro.config.js
+++ metro.config.js
@@ -3,8 +3,8 @@ const MetroConfig = require('@ui-kitten/metro-config')
 const evaConfig = {
   evaPackage: '@eva-design/eva',
   // Optional, but may be useful when using mapping customization feature.
-  customMappingPath: './src/mapping.json',
-  watch: process.env.CI !== 'true'
+  customMappingPath: './src/mapping.json'
+  // watch: process.env.CI !== 'true'
 }
 
 module.exports = MetroConfig.create(evaConfig, {
